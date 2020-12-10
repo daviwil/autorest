9a828dd0bd7670eead4a33b13ec1a51d670a1d67
David Wilson <daviwil@microsoft.com>
2020-12-09 16:03:16 -0800

    WIP: Pass IFileSystem to oai2-to-oai3 converter

--- core/lib/pipeline/plugins/conversion.ts
+++ core/lib/pipeline/plugins/conversion.ts
@@ -1,10 +1,11 @@
 import { convertOAI2toOAI3 } from '../../openapi/conversion';
 import { createPerFilePlugin, PipelinePlugin } from '../common';
+import { IFileSystem } from '@azure-tools/datastore';
 
 /* @internal */
-export function createSwaggerToOpenApi3Plugin(): PipelinePlugin {
+export function createSwaggerToOpenApi3Plugin(fileSystem?: IFileSystem): PipelinePlugin {
   return createPerFilePlugin(async () => async (fileIn, sink) => {
-    const fileOut = await convertOAI2toOAI3(fileIn, sink);
+    const fileOut = await convertOAI2toOAI3(fileIn, sink, fileSystem);
     return sink.Forward(fileIn.Description, fileOut);
   });
 }
